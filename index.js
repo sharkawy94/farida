4527fb8a2dfea7f511d3412ef6271d29c41ee6fa
omarsharkawy <sharkawy@fatura-egypt.com>
2024-04-30 02:41:14 +0200

    Created login page UI

---
+++ index.js
@@ -0,0 +1,8 @@
+function AddAnimation(){
+	const button = document.getElementsByClassName("button")[0];
+	button.classList.add("button-animation");
+}
+function RemoveAnimation(){
+	const button = document.getElementsByClassName("button")[0];
+	button.classList.remove("button-animation");
+}
