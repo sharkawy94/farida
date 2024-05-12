0ee5ec1d501b1f32d104593b30c3bfff829690f5
omarsharkawy <sharkawy@fatura-egypt.com>
2024-05-13 00:06:35 +0200

    Rendering html

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
-function AddAnimation(){
-	const button = document.getElementsByClassName("button")[0];
-	button.classList.add("button-animation");
+// function AddAnimation(){
+// 	const button = document.getElementsByClassName("button")[0];
+// 	button.classList.add("button-animation");
+// 	setTimeout(()=>{button.classList.remove("button-animation");},200)
+// }
+const items = [
+	{
+		title: "T-Shirt",
+		color: "Green",
+		gender: "Male",
+		imageUrl: "url of image"
+	},
+	{
+		title: "Short",
+		color: "Blue",
+		gender: "Female",
+		imageUrl: "url of image"
+	},
+	{
+		title: "T-Shirt",
+		color: "Green",
+		gender: "Male",
+		imageUrl: "url of image"
+	},
+	{
+		title: "Short",
+		color: "Blue",
+		gender: "Female",
+		imageUrl: "url of image"
+	}
+]
+
+function renderItemsContainer(filterObj) {
+	console.log(filterObj)
+	let filteredItems = items;
+	if(filterObj)
+		filteredItems =	filteredItems.filter(item => item[filterObj.filterCriteria] == filterObj.filterValue)
+
+	const itemsHtml = filteredItems.map(item=>{
+		return `<div class="item-title">${item.title}</div>
+						<div class="item-color">${item.color}</div>
+						<div class="item-gender">${item.gender}</div>
+						<div class="image"><img src="${item.imageUrl}" alt=""></div>
+						<div class="add-to-cart"><button>Add</button></div>`
+	}).join('')
+	
+	let itemsContainer = document.getElementById("items-container");
+	
+	itemsContainer.innerHTML = itemsHtml;	
 }
-function RemoveAnimation(){
-	const button = document.getElementsByClassName("button")[0];
-	button.classList.remove("button-animation");
-}
+
+window.addEventListener('load', function() {
+
+renderItemsContainer(null)
+
+})
